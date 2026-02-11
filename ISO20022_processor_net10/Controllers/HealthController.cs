3433909906ab7bf6a2e11d26d9cebd2701d4edf1
Erwin Wolff <erwinwolff@hotmail.com>
2026-02-11 23:48:22 +0100

    Basic PAIN files checkin from ISO committee

---
+++ ISO20022_processor_net10/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ISO20022_processor_net10.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            return Ok("Healthy");
+        }
+    }
+}
